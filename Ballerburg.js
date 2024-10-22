602f0352538273418a947b78328a6fe4d319011e
MiaGMB <mre42348@stud.hs-furtwangen.de>
2024-10-22 17:44:37 +0200

    added sliders with jirkas held, chnged stuff with jirkas help

--- Ballerburg.js
+++ Ballerburg.js
@@ -1,40 +1,58 @@
 "use strict";
-let canvas = document.querySelector("canvas");
-let ctx = canvas.getContext("2d");
+const canvas = document.getElementsByTagName("canvas")[0];
+const ctx = canvas.getContext("2d");
 canvas.height = innerHeight;
+document.addEventListener("keydown", hndKeydown);
+let sliderManipulate = document.querySelector("input#AngleKeyboard");
+drawMountain();
+function hndKeydown(_event) {
+    console.log(_event);
+    if (_event.code == "ArrowUp")
+        sliderManipulate = document.querySelector("input#AngleKeyboard");
+    if (_event.code == "ArrowDown")
+        sliderManipulate = document.querySelector("input#GunpowerKeyboard");
+    sliderManipulate.value = (parseInt(sliderManipulate.value) + 1) + "";
+    console.log(sliderManipulate);
+}
 function randomHeightMountain() {
     return Math.floor(Math.random() * 500);
+}
+function drawMountain() {
+    ctx.beginPath();
+    ctx.moveTo(0, 200);
+    ctx.lineTo(150, 0);
+    ctx.lineTo(300, 200);
+    ctx.lineTo(0, 200);
+    ctx.fillStyle = "green";
+    ctx.fill();
 }
 function randomHeightCanon() {
     return Math.floor(Math.random() * 200);
 }
-function addVectors(v1, v2) {
+function addVectors(_v1, _v2) {
     return {
-        x: v1.x + v2.x,
-        y: v1.y + v2.y
+        x: _v1.x + _v2.x,
+        y: _v1.y + _v2.y
     };
 }
-function updateBall(ball) {
-    ball.position = addVectors(ball.position, ball.velocity);
+function updateBall(_ball) {
+    _ball.position = addVectors(_ball.position, _ball.velocity);
 }
 function checkBallCollision() {
 }
-function drawBall(ctx, ball) {
-    ctx.beginPath();
-    ctx.arc(ball.position.x, ball.position.y, ball.radius, 0, Math.PI * 2);
-    ctx.fillStyle = ball.color;
-    ctx.fill();
-    ctx.closePath();
+function drawBall(_ctx, _ball) {
+    _ctx.beginPath();
+    _ctx.arc(_ball.position.x, _ball.position.y, _ball.radius, 0, Math.PI * 2);
+    _ctx.fillStyle = _ball.color;
+    _ctx.fill();
+    _ctx.closePath();
 }
 function drawCanon() {
 }
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
-    updateBall(ball);
+    // updateBall(ball);
     //checkBallCollision(ball, canvas.width, canvas.height); 
-    drawBall(ctx, ball);
+    // drawBall(ctx, ball);
     requestAnimationFrame(animate); // Recursively call the animate function for the next frame
 }
-function drawMountain() {
-}
-//Canvas
--- Ballerburg.ts
+++ Ballerburg.ts
@@ -1,13 +1,8 @@
-const canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
-const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
-
-canvas.height = innerHeight;
-
-interface Mountain{
-    height: number;
-    yPos: number;
-    xPosL: number;//linke untere Ecke der Dreiecks
-    xPosR: number;//rechte''
+interface Mountain {
+  height: number;
+  yPos: number;
+  xPosL: number;//linke untere Ecke der Dreiecks
+  xPosR: number;//rechte''
 
 }
 
@@ -17,79 +12,99 @@ interface Vector {
 }
 
 interface Canon {
-    color: string;
-    yPos: number;
-    power : number;
-    direction : Vector
+  color: string;
+  yPos: number;
+  power: number;
+  direction: Vector
 
 }
 
 interface Ball {
-    color: string;
-    radius: number;
-    position: Vector;
-    velocity : Vector;
-    active : boolean;
+  color: string;
+  radius: number;
+  position: Vector;
+  velocity: Vector;
+  active: boolean;
 }
 
-function randomHeightMountain():number {
 
-   return Math.floor(Math.random() * 500);
+const canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
+const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
+
+canvas.height = innerHeight;
+
+document.addEventListener("keydown", hndKeydown);
+
+let sliderManipulate: HTMLInputElement = document.querySelector("input#AngleKeyboard")!;
+
+drawMountain();
+
+function hndKeydown(_event: KeyboardEvent): void {
+  console.log(_event);
+  if (_event.code == "ArrowUp") 
+    sliderManipulate = document.querySelector("input#AngleKeyboard")!;
+
+  if(_event.code == "ArrowDown")
+    sliderManipulate = document.querySelector("input#GunpowerKeyboard")!;
+
+  sliderManipulate.value =  (parseInt(sliderManipulate.value) + 1) + "";
+
+  console.log(sliderManipulate);  
+
 }
 
-function drawMountain() {
+function randomHeightMountain(): number {
+  return Math.floor(Math.random() * 500);
+}
+
+function drawMountain(): void {
   ctx.beginPath();
-  ctx.moveTo(50, 0);
-  ctx.lineTo(150, 150);
-  ctx.lineTo(250, 0);
-  ctx.lineTo(50, 0);
+  ctx.moveTo(0, 200);
+  ctx.lineTo(150, 0);
+  ctx.lineTo(300, 200);
+  ctx.lineTo(0, 200);
   ctx.fillStyle = "green";
+  ctx.fill();
 }
 
-function randomHeightCanon():number {
-    return Math.floor(Math.random() * 200);
-
+function randomHeightCanon(): number {
+  return Math.floor(Math.random() * 200);
 }
 
-function addVectors(v1: Vector, v2: Vector): Vector {
-    return {
-      x: v1.x + v2.x,
-      y: v1.y + v2.y
-    };
-  }
-
-function updateBall(ball: Ball): void {
-    ball.position = addVectors(ball.position, ball.velocity);
-  }
-
-function checkBallCollision():void {
-
+function addVectors(_v1: Vector, _v2: Vector): Vector {
+  return {
+    x: _v1.x + _v2.x,
+    y: _v1.y + _v2.y
+  };
 }
 
-function drawBall(ctx: CanvasRenderingContext2D, ball: Ball): void {
-    ctx.beginPath();
-    ctx.arc(ball.position.x, ball.position.y, ball.radius, 0, Math.PI * 2);
-    ctx.fillStyle = ball.color;
-    ctx.fill();
-    ctx.closePath();
-  }
+function updateBall(_ball: Ball): void {
+  _ball.position = addVectors(_ball.position, _ball.velocity);
+}
 
-function drawCanon():void{
+function checkBallCollision(): void {
+}
+
+function drawBall(_ctx: CanvasRenderingContext2D, _ball: Ball): void {
+  _ctx.beginPath();
+  _ctx.arc(_ball.position.x, _ball.position.y, _ball.radius, 0, Math.PI * 2);
+  _ctx.fillStyle = _ball.color;
+  _ctx.fill();
+  _ctx.closePath();
+}
+
+function drawCanon(): void {
 
 }
 
 function animate(): void {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
-  
-    updateBall(ball);
-    //checkBallCollision(ball, canvas.width, canvas.height); 
-    drawBall(ctx, ball); 
-  
-    requestAnimationFrame(animate); // Recursively call the animate function for the next frame
-  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
 
-  function drawMountain():void{
+  // updateBall(ball);
+  //checkBallCollision(ball, canvas.width, canvas.height); 
+  // drawBall(ctx, ball);
 
-  }
+  requestAnimationFrame(animate); // Recursively call the animate function for the next frame
+}
 
 
