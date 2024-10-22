fbc594aedee27edbe7754e4b06c2dd04b2b77b83
NxNelly <stadelnelly@gmx.de>
2024-10-22 11:28:25 +0200

    ts

--- Ballerburg.ts
+++ Ballerburg.ts
@@ -1,2 +1,25 @@
 let canvas: HTMLCanvasElement = document.querySelector("canvas");
 let crc2: CanvasRenderingContext2D = canvas.getContext("2d");
+
+interface Canon {
+    color: string;
+    yPos: number;
+    power : number;
+    direction : vector
+
+}
+
+interface Ball {
+    position: Vector;
+    velocity : Vector;
+    active : boolean;
+}
+
+function randomHeightMountain(){
+
+    Math.floor(Math.random() * 500);
+}
+
+function randomHeightCanon(){
+    Math.floor(Math.random() * 200);
+}
