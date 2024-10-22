454ae80d2c18b019cb6e4586c98e8420b6a90d53
NxNelly <stadelnelly@gmx.de>
2024-10-22 12:18:43 +0200

    first ball functions

--- Ballerburg.ts
+++ Ballerburg.ts
@@ -1,5 +1,10 @@
-let canvas: HTMLCanvasElement = document.querySelector("canvas");
-let crc2: CanvasRenderingContext2D = canvas.getContext("2d");
+let canvas: HTMLCanvasElement = document.querySelector("canvas") ;
+let ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
+
+canvas.height = innerHeight;
+canvas.width = 600;
+canvas.
+
 
 interface Canon {
     color: string;
@@ -10,6 +15,8 @@ interface Canon {
 }
 
 interface Ball {
+    color: string;
+    radius: number;
     position: Vector;
     velocity : Vector;
     active : boolean;
@@ -23,6 +30,43 @@ function randomHeightMountain(){
 function randomHeightCanon(){
     Math.floor(Math.random() * 200);
 }
+
+function addVectors(v1: Vector, v2: Vector): Vector {
+    return {
+      x: v1.x + v2.x,
+      y: v1.y + v2.y
+    };
+  }
+
+function updateBall(ball: Ball): void {
+    ball.position = addVectors(ball.position, ball.velocity);
+  }
+
+function checkBallBounds() {
+
+}
+
+function drawBall(ctx: CanvasRenderingContext2D, ball: Ball): void {
+    ctx.beginPath();
+    ctx.arc(ball.position.x, ball.position.y, ball.radius, 0, Math.PI * 2);
+    ctx.fillStyle = ball.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+
+function drawCanon(){
+
+}
+
+function animate(): void {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
+  
+    updateBall(ball); // Update ball position
+    checkBallBounds(ball, canvas.width, canvas.height); // Check and handle collisions with canvas borders
+    drawBall(ctx, ball); // Draw the ball on canvas
+  
+    requestAnimationFrame(animate); // Recursively call the animate function for the next frame
+  }
 
 
 //Canvas
